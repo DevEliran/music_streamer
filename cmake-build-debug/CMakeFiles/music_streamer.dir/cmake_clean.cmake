e62af848e1feff9192a19b63a7f9ddd5da790e39
Eliran T <dev.eliran96@gmail.com>
2020-04-25 18:30:50 +0300

    a

---
+++ cmake-build-debug/CMakeFiles/music_streamer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/music_streamer.dir/main.cpp.obj"
+  "libmusic_streamer.dll.a"
+  "music_streamer.exe"
+  "music_streamer.exe.manifest"
+  "music_streamer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/music_streamer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
